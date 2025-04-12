eebca0f6c35b861001028304e1446976e96c4c56
Danhyeye <trancongdanhyeye@gmail.com>
2025-04-12 14:02:15 +0700

    🐛 fix(app): fix deploy prerendered about (admin)

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,12 +1,9 @@
 import AdminDashboardClient from "./AdminDashboardClient";
-import AdminDashboardSkeleton from "./LoadingState";
-import { Suspense } from "react";
 
 export default function AdminDashboardPage() {
   return (
-    <Suspense fallback={<AdminDashboardSkeleton />}>
       <AdminDashboardClient />
-    </Suspense>
+ 
   );
 }
 
